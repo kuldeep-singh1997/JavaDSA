41e0a335c1a830289a82c0250515b54115869378
kuldeep-singh1997 <104989330+kuldeep-singh1997@users.noreply.github.com>
2024-07-21 05:21:47 +0000

    Arrays

--- BinarySearch.java
+++ BinarySearch.java
@@ -1,42 +1,27 @@
-import java.util.ArrayList;
-import java.util.Collections;
-
-import javafx.print.Collation;
-
 class BinarySearch{
-    public static void main(String[] args) {
-        // int arr[]=new int[]{1,2,3,4,5,6,7,8};
-        // int start=0;
-        // int end=arr.length-1;
-        // int mid=(start+end)/2;
-        // int key=100;
-        // int index=-1;
-        // while(start<end){
-        //     System.out.println("--------"+arr[mid]);
-        //     if(arr[mid]==key){
-        //         System.out.println("Insde");
-        //         index=mid;
-        //         break;
-        //     }
-        //     if(arr[mid]>key)
-        //         end=mid-1;
-		// 	else
-		// 		start=mid+1;
-			
-		// 	mid=(start+end)/2;
-        // }
-        // System.out.println("Index is --   "+index);
-        int arr[]=new int[]{1,2,3,4,5};
+    public static int search(int arr[],int target){
         int start=0;
         int end=arr.length-1;
-        ArrayList<Integer> arList=new ArrayList<>();
-        for(int i:arr){
-            arList.add(Integer.valueOf(i));
+        int mid=(start+end)/2;
+        int ans=-1;
+        while(start<=end){
+            if(arr[mid]==target){
+                ans=mid;
+                return ans;
+            }
+            else if(target>arr[mid]){
+                start=mid+1;
+            }
+            else{
+                end=mid-1;
+            }
+            mid=(start+end)/2;
         }
-        Collections.reverse(arList);
-        for(int i=0;i<arList.size();i++){
-           System.out.println("----------"+arList.get(i));
-        }
-        
+        return ans;
+    }
+    public static void main(String a[]){
+        int arr[]={1,2,3,4,5,6,7,8};
+        System.out.println("Found at index "+search(arr,6));
+        System.out.println("Found at index "+search(arr,7));
     }
 }
